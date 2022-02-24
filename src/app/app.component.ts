98618411d1f6cf15e235b9e63b79821170493ab9
Mursaat <aurelien.dussauge@gmail.com>
2022-02-24 13:45:10 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  VERSION,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  @HostBinding('class.my-app')
+  public hasBoundClass = true;
+
   name = 'Angular ' + VERSION.major;
 }
